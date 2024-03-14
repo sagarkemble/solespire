89541f0c710b3447a794eb2d3ae2aae1d4003c34
Sagar kemble <131292159+sagarkemble@users.noreply.github.com>
2024-03-14 17:25:59 +0530

    Delete common.js

--- common.js
+++
@@ -1,9 +0,0 @@
-function adjustBrowserZoom() {
-    var devicePixelRatio = window.devicePixelRatio || 1;
-    var displaySize = devicePixelRatio * 100;
-    var desiredZoomLevel = 100 * (100 / displaySize);
-    document.body.style.zoom = desiredZoomLevel + '%';
-  }
-  
-  window.addEventListener('load', adjustBrowserZoom);
-  
